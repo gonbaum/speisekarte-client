5ca315846389086b8841c49028ec3ee786d249cc
gonbaum <nouvellegon@gmail.com>
2020-07-01 18:02:15 +0200

    Added filter by category

--- src/App.js
+++ src/App.js
@@ -5,31 +5,38 @@ import Category from './components/Category'
 
 function App() {
 
-  // State for loaded items:
-
+  // State: Menu items:
   const [menuItems, setMenuItems] = useState([])
 
-  // Load categories in state: 
-  const [categories, setCategories] = useState(["All"])
-  // Selected category in state:
-  const [selectedCat, setSelectedCat] = useState(["All"])
+  // State: Categories available: 
+  const [categories, setCategories] = useState([""])
+
+  // State: Selected category:
+  const [selectedCat, setSelectedCat] = useState("All")
 
   // Fetch initial notes:
 
   useEffect(() => {
         setMenuItems(items)
         const itemCategories = items.map(item => item.category)
-        setCategories([...new Set(itemCategories), "All"])
+        setCategories([...new Set(itemCategories)])
+
   }, [])
-  
+
+    const handleClick = (cat) => {
+      setSelectedCat(cat)
+      console.log(cat)
+    }
 
   return (
     <div className="App">
       <header className="App-header">
       <h1>Speisekarte</h1>
-      {categories.map(cat => <button onClick={(cat) => setSelectedCat(cat)}>{cat} </button>)}
-      <Category items={menuItems} category="food"/>
-      <Category items={menuItems} category="coffee"/>
+      <span>Categories:
+        {categories.map((cat, i) => <button key={i} onClick={ () => handleClick(cat) }>{cat} </button>)}
+        <button onClick={ () => handleClick("All") }>All</button>
+      </span>
+      {selectedCat !== "All" ? <Category items={menuItems} category={selectedCat}/> : categories.map((cat, i) => <Category key={i} items={menuItems} category={cat}/> )}
       </header>
     </div>
   )
