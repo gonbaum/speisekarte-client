ad530a709c7a7f87365f0474866c61952a0ada4b
gonbaum <nouvellegon@gmail.com>
2020-07-01 16:43:27 +0200

    Render by categories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import { items } from './db.json'
-import Item from './components/Item'
+import Category from './components/Category'
 
 function App() {
 
@@ -19,11 +19,8 @@ function App() {
     <div className="App">
       <header className="App-header">
       <h1>Speisekarte</h1>
-      <ul>
-        {menuItems.map((itemData, i) =>
-          <Item itemData={itemData} key={i} /> 
-        )}
-      </ul>
+      <Category items={menuItems} category="food"/>
+      <Category items={menuItems} category="coffee"/>
       </header>
     </div>
   )
--- src/components/Item.js
+++ src/components/Item.js
@@ -2,19 +2,17 @@ import React from 'react'
 
 const Item = ({ itemData }) => {
     return(
-        <div className="menu-item">
+        <li className="menu-item">
             <div className="item-data">
-                <li>
                     <span>{itemData.name}</span>
                     <p>{itemData.description}</p>
                     <p>Price: {itemData.price} Eur</p>
-                </li>
             </div>
             <div className="item-image">
                 <img className="item-image" src={itemData.img} alt={itemData.name}></img>
                 <div className="highligted" style={{ display: itemData.highlighted ? null : 'none'}}>Highlighted</div>
             </div>
-        </div>
+        </li>
 
     )
 }
