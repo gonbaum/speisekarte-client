c2e02fe949cc060642f191b7a99efc122595327b
gonbaum <nouvellegon@gmail.com>
2020-07-01 17:07:54 +0200

    Showing categories buttons

--- src/App.js
+++ src/App.js
@@ -9,16 +9,25 @@ function App() {
 
   const [menuItems, setMenuItems] = useState([])
 
+  // Load categories in state: 
+  const [categories, setCategories] = useState(["All"])
+  // Selected category in state:
+  const [selectedCat, setSelectedCat] = useState(["All"])
+
   // Fetch initial notes:
 
   useEffect(() => {
         setMenuItems(items)
+        const itemCategories = items.map(item => item.category)
+        setCategories([...new Set(itemCategories), "All"])
   }, [])
   
+
   return (
     <div className="App">
       <header className="App-header">
       <h1>Speisekarte</h1>
+      {categories.map(cat => <button onClick={(cat) => setSelectedCat(cat)}>{cat} </button>)}
       <Category items={menuItems} category="food"/>
       <Category items={menuItems} category="coffee"/>
       </header>
