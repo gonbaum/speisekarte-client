d7ad38a96c5a9ee0224d34ba24962deac2d8b37c
gonbaum <nouvellegon@gmail.com>
2020-07-02 11:24:18 +0200

    Updates

---
+++ precache-manifest.6459e997a07dbff280728b93523a59a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83adf26f3b47fc451f45d66e706cfa4f",
+    "url": "/speisekarte-client/public/index.html/index.html"
+  },
+  {
+    "revision": "aa57007dd1bb78dbc916",
+    "url": "/speisekarte-client/public/index.html/static/css/main.0589a2a8.chunk.css"
+  },
+  {
+    "revision": "2a221f2a07f43065758e",
+    "url": "/speisekarte-client/public/index.html/static/js/2.6ce9a4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speisekarte-client/public/index.html/static/js/2.6ce9a4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa57007dd1bb78dbc916",
+    "url": "/speisekarte-client/public/index.html/static/js/main.55ac5c44.chunk.js"
+  },
+  {
+    "revision": "d58ac0cfd234c5473a5b",
+    "url": "/speisekarte-client/public/index.html/static/js/runtime-main.e13c1494.js"
+  }
+]);
