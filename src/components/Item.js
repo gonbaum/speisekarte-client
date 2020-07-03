90b64f300976a98ca0bf914439ed257b2835d2cc
gonbaum <nouvellegon@gmail.com>
2020-07-03 10:15:02 +0200

    New styling for price

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,15 +4,17 @@ const Item = ({ itemData }) => {
     return(
         <li className="menu-item">
             <div className="item-data">
-                    <h2>{itemData.name}&emsp;&emsp;{itemData.price} €</h2>
+                    <h2>{itemData.name}</h2>
                     <p>{itemData.description}</p>
-                    <p></p>
+                    <p>{itemData.price} €</p>
             </div>
             <div className="item-image" style={{display: 'none'}}>
                 <img className="item-image" src={itemData.img} alt={itemData.name}></img>
                 <div className="highligted" style={{ display: itemData.highlighted ? null : 'none'}}>Highlighted</div>
             </div>
         </li>
+        
+
     )
 }
 
